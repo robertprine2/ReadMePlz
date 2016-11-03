34dc5febf9dc02664c9457682e5b44fe96ff67c2
Robert Prine <robertprine2@yahoo.com>
2016-11-02 22:34:32 -0400

    adding oauth slack

--- server.js
+++ server.js
@@ -1,12 +1,25 @@
 // requires express and body-parser
+var config = require('./log.js');
 var express = require('express');
 var bodyParser = require('body-parser');
 var Slack = require('slack-node');
+var passport-slack = require('passport-slack');
 
 webhookUri = "https://hooks.slack.com/services/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";
  
 slack = new Slack();
 slack.setWebhook(webhookUri);
+
+passport.use(new SlackStrategy({
+        clientID: config.sauth.clientID,
+        clientSecret: config.sauth.clientSecret
+    },
+    function(accessToken, refreshToken, profile, done) {
+        User.findOrCreate({ SlackId: profile.id }, function (err, user) {
+        return done(err, user);
+        });
+    }
+));
 
 //requiring and setting up mongo database/collections
 var mongojs = require('mongojs');
@@ -43,6 +56,16 @@ app.get('/',  function(req, res) {
     res.sendFile(__dirname + '/public/views/index.html');
 
 });
+
+app.get('/auth/slack',
+    passport.authorize('slack'));
+
+app.get('/auth/slack/callback', 
+    passport.authorize('slack', { failureRedirect: '/login' }),
+    function(req, res) {
+      // Successful authentication, redirect home.
+      res.redirect('/');
+    });
 
 app.post('/survey', function(req, res) {
     var userName = req.body.user_name;
